994bab75d64602f5e12ed38bb1b7c0312054ad4c
Raunaq <raunaq.singh@clickpost.in>
2021-07-15 22:29:41 +0530

    feat: added heroku url to allowed hosts

--- dps_recipes/settings.py
+++ dps_recipes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4p#jeiyi+x-)fxc2k7j_)p##xdy-b1u%lx!)$66r^+&o)n_lh%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dps-challenge.herokuapp.com']
 
 
 # Application definition
