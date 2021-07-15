d48fbde8336cd85cc27630b52a3638515a08f2a3
Raunaq <raunaq.singh@clickpost.in>
2021-07-15 22:17:57 +0530

    feat: added static_url in settings.py

--- dps_recipes/settings.py
+++ dps_recipes/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "django_static")
 STATIC_URL = '/static/'
 
 # Default primary key field type
